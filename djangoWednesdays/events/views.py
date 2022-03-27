c19a26c79324bbedf19e252a84b2069a7e8c9216
Sarah <aga.wojsznis@gmail.com>
2022-03-27 23:34:57 +0200

    calendar

--- djangoWednesdays/events/views.py
+++ djangoWednesdays/events/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
 
 
 # Create your views here.
-def home(request):
-    return render(request, 'home.html', {})
+def home(request, year, month):
+    name = "Tony"
+    month = month.capitalize()
+    month_number = int(list(calendar.month_name).index(month))
+
+    cal = HTMLCalendar().formatmonth(year, month_number)
+    now_year = datetime.now().year
+    time = datetime.now().strftime('%I:%M:%S %p')
+
+    return render(request, 'home.html', {
+        "first_name": name,
+        "year": year,
+        "month": month,
+        "month_number": month_number,
+        "cal": cal,
+        "now_year": now_year,
+        "time": time,
+    })
+
